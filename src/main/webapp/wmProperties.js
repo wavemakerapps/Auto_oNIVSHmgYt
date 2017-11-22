11bdce977ad5332863527df329c8a0c548166a8c
ZMlWxmArSN <automate1@wavemaker.com>
2017-11-22 10:04:58 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_GtIPBZVfVf",
+  "homePage" : "Main",
+  "name" : "Auto_GtIPBZVfVf",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
